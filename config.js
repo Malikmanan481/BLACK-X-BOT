7530e45e31843a15efced02ae301af4dff80f950
Malikmanan481 <gpstrace6@gmail.com>
2024-12-12 00:37:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "2i41FKZY#9uv3z2mn04z5TNtU_doXNIqgHJKHxAmU36C29VixDhI",
+    SESSION_ID: process.env.SESSION_ID || "enter your session",
     CAPTION: process.env.CAPTION || "POWERED BY BLACK-X",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
